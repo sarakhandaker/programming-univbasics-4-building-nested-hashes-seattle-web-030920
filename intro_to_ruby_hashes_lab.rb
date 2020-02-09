315213d142f6da633ad5df10740e06d4624f411e
Sara Khandaker <kttsara@hotmail.com>
2020-02-09 18:44:25 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -10,7 +10,7 @@ def monopoly_with_third_tier
   	hash={railroads: 
   	{ pieces: 4, 
   	rent_in_dollars: {"1":25, "2":50, "3":100, "4":200},
-  	  names: ["reading", "g", "h", "i"]
+  	  names: {"reading":, "g":, "h":, "i":}
   	}
   	}
 end
